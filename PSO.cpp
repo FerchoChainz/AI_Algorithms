a7a184935c4409fcfc849e8ecc74cf7d5a97e116
Fernando <107149159+FerchoChainz@users.noreply.github.com>
2023-09-16 09:14:03 -0600

    Update PSO.cpp

--- PSO.cpp
+++ PSO.cpp
@@ -11,17 +11,18 @@
 #define minx -10.0
 #define maxx 10.0
 
-// Estructura para representar una partícula.
+// Estructura para representar una partÃ­cula.
 typedef struct {
     double position;
     double velocity;
     double bestPos;
+    double fitness;
     double bestFitness;
 } Particle;
 
-// Función objetivo que queremos minimizar (puedes cambiarla según tus necesidades).
+// FunciÃ³n objetivo que queremos minimizar (puedes cambiarla segÃºn tus necesidades).
 double funcion_objetivo(double x) {
-    return x * x;  // Ejemplo: minimizar la función x^2.
+    return x * x;  // Ejemplo: minimizar la funciÃ³n x^2.
 }
 
 int main() {
@@ -29,7 +30,7 @@ int main() {
     double best_fitness_swarm = INT_MAX;
     double best_pos_swarm;
 
-    // Inicialización de las partículas.
+    // InicializaciÃ³n de las partÃ­culas.
     for (int i = 0; i < N; i++) {
         swarm[i].position = (double)rand() / RAND_MAX * (maxx - minx) + minx;
         swarm[i].velocity = 0.0;
@@ -44,22 +45,22 @@ int main() {
             double r1 = (double)rand() / RAND_MAX;
             double r2 = (double)rand() / RAND_MAX;
 
-            // Actualizar la velocidad de la partícula.
+            // Actualizar la velocidad de la partÃ­cula.
             swarm[i].velocity = w * swarm[i].velocity +
                                 r1 * c1 * (swarm[i].bestPos - swarm[i].position) +
                                 r2 * c2 * (best_pos_swarm - swarm[i].position);
 
-            // Actualizar la posición de la partícula.
+            // Actualizar la posiciÃ³n de la partÃ­cula.
             swarm[i].position += swarm[i].velocity;
 
-            // Asegurarse de que la posición esté dentro del rango [minx, maxx].
+            // Asegurarse de que la posiciÃ³n estÃ© dentro del rango [minx, maxx].
             if (swarm[i].position < minx) {
                 swarm[i].position = minx;
             } else if (swarm[i].position > maxx) {
                 swarm[i].position = maxx;
             }
 
-            // Actualizar el mejor de la partícula y el mejor global.
+            // Actualizar el mejor de la partÃ­cula y el mejor global.
             if (swarm[i].fitness < swarm[i].bestFitness) {
                 swarm[i].bestFitness = swarm[i].fitness;
                 swarm[i].bestPos = swarm[i].position;
@@ -73,7 +74,7 @@ int main() {
     }
 
     printf("Mejor valor encontrado: %lf\n", best_fitness_swarm);
-    printf("Mejor posición encontrada: %lf\n", best_pos_swarm);
+    printf("Mejor posiciÃ³n encontrada: %lf\n", best_pos_swarm);
 
     return 0;
 }
